2c59df0dba75354b02d06aacd2f5aca38702a2c2
Artur Carvalho <arturpedroso5050@gmail.com>
2024-11-26 20:28:13 -0300

    inicia o projeto

---
+++ src/test/java/beneficiarios/cadastro/Plano_Saude/PlanoSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package beneficiarios.cadastro.Plano_Saude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
