14e2c6a032ed4e69729d6d279b886b6da73a209c
Artur Carvalho <arturpedroso5050@gmail.com>
2024-11-27 09:22:08 -0300

    implementa contrato da API

--- src/main/java/beneficiarios/cadastro/Plano_Saude/beneficiario/application/api/BeneficiarioAPI.java
+++ src/main/java/beneficiarios/cadastro/Plano_Saude/beneficiario/application/api/BeneficiarioAPI.java
@@ -1,10 +1,18 @@
 package beneficiarios.cadastro.Plano_Saude.beneficiario.application.api;
 
+import beneficiarios.cadastro.Plano_Saude.beneficiario.domain.Beneficiario;
+import beneficiarios.cadastro.Plano_Saude.documento.domain.Documento;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/beneficiario")
-public class BeneficiarioAPI {
+public interface BeneficiarioAPI {
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    BeneficiarioResponse cadastrarBeneficiario(@RequestBody @Valid BeneficiarioRequest beneficiarioRequest);
 
 }
