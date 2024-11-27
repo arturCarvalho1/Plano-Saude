6a9d0ffadb6a6629d50182b895bbd7682e70d8d9
Artur Carvalho <arturpedroso5050@gmail.com>
2024-11-26 21:07:16 -0300

    implementa api de teste

--- src/main/java/beneficiarios/cadastro/Plano_Saude/PlanoSaudeApplication.java
+++ src/main/java/beneficiarios/cadastro/Plano_Saude/PlanoSaudeApplication.java
@@ -2,10 +2,19 @@ package beneficiarios.cadastro.Plano_Saude;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PlanoSaudeApplication {
 
+	@GetMapping
+	public String getHometeste(){
+		return "Plano saude - API Home ";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PlanoSaudeApplication.class, args);
 	}
